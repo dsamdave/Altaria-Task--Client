897280f5e4d2e9874c3892ec7fc0a9efceb3c79b
David Sampson <davidchosen33@gmail.com>
2024-12-06 21:53:21 +0100

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,29 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff", // Prevent MIME type sniffing
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; img-src 'self' data:; script-src 'self'; style-src 'self' 'unsafe-inline';", // Adjust CSP based on your app's needs
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin", // Control referrer information
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
